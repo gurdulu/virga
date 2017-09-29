a3d0666d1e06240c9d436afeb27817ad92057d90
gurdulu <macgurd@gmail.com>
2017-09-29 23:18:19 +0100

    Version 0.0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='virga',
-    version='0.0.6',
+    version='0.0.7',
     description='Analysing your Cloud infrastructure before the rain falls to the ground',
     long_description=io.open('README.rst', encoding='utf-8').read(),
     classifiers=[
